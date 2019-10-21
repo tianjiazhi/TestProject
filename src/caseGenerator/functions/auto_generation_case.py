e3a7748f359c4af8e986d8e5c998713bc2470054
YunFan <tianjiazhi615@163.com>
2019-10-22 07:50:36 +0800

    v1.0.1 第二次提交，修改auto_generation_case.py

--- src/caseGenerator/functions/auto_generation_case.py
+++ src/caseGenerator/functions/auto_generation_case.py
@@ -19,10 +19,18 @@ log = logger
 
 class WriteCellLogic():
 
-    def __init__(self,w):
-        self.write = w
-        self.write_line = 0          # 初始化当前工作表的行数
-        self.write_columns = 0       # 初始化当前工作表的列数
+    def __init__(self,write,sheet:str,rows:int):
+
+        # 读取excel相关的属性
+        self.sheet = sheet          # 读取当前文件的工作表名称
+        self.rows = rows            # 读取当前工作表的总行数
+        self.line = None            # 读取当前行数[row]的内容
+        self.row = 0                # 读取当前内容所在的行数
+
+        # 写入excel相关的属性
+        self.write = write          # 创建一个写文件实例
+        self.write_line = 0         # 初始化写入当前工作表的行数
+        self.write_columns = 0      # 初始化写入当前工作表的列数
 
     def write_excel_title(self):
         """
@@ -70,122 +78,92 @@ class WriteCellLogic():
         log.info("向第{0}行写入标题结束，进行换行后行号变成{1}《《《《《《《《《《《《《《《《《《".format(self.write_line -1,self.write_line))
         log.info("")
 
+    def run_write_cell_logic(self,line,row):
+        self.line = line
+        self.row = row
 
+        ##############################对模板中填写的信息进行非法法性校验####################################################
+        if self.row != 0 and self.line[0].upper() not in ("YES", "NO"):
+            error = "数据源模板:工作表 {} 中的单元格 A{} 即[ifCheckValidity]字段的值【{}】系统未定义或未填写，请检查后重试！".format(self.sheet,
+                                                                                                self.row + 1,
+                                                                                                self.line[0])
+            log.error(error)
+            raise ValueError(error)
 
+        if self.row != 0 and self.line[1].upper() not in ("YES", "NO"):
+            error = "数据源模板:工作表 {} 中的单元格 B{} 即[ifCheckSpecial]字段的值【{}】系统未定义或未填写，请检查后重试！".format(self.sheet, self.row + 1,
+                                                                                               self.line[1])
+            log.error(error)
+            raise ValueError(error)
 
-def run_write_excel_logic(line,row,sheet,write_cell_logic):
-    """
+        if self.row != 0 and self.line[2].upper() not in ("YES", "NO"):
+            error = "数据源模板:工作表 {} 中的单元格 C{} 即[ifCheckBoundary]字段的值【{}】系统未定义或未填写，请检查后重试！".format(self.sheet,
+                                                                                                self.row + 1,
+                                                                                                self.line[2])
+            log.error(error)
+            raise ValueError(error)
 
-    :param line: 读到当前行的内容
-    :param row:  行数
-    :param write_cell_logic:将读到的数据进行逻辑化处理后
-    :return:
-    """
-    ##############################对模板中填写的信息进行非法法性校验####################################################
+        if self.row != 0 and not self.line[3]:  # moduleName字段为必填项，当判定为空时，抛出错误。
+            error = "数据源模板:工作表 {} 中的单元格 D{} 即[moduleName]字段的值【{}】不能为空，请填写后重试！".format(self.sheet, self.row + 1,
+                                                                                      self.line[3])
+            log.error(error)
+            raise ValueError(error)
 
-    if row != 0 and line[0].upper() not in ("YES","NO"):
-        error = "数据源模板:工作表 {} 中的单元格 A{} 即[ifCheckValidity]字段的值【{}】系统未定义或未填写，请检查后重试！".format(sheet,row+1, line[0])
-        log.error(error)
-        raise ValueError(error)
+        if self.row != 0 and not self.line[4]:  # interfaceName字段为必填项，当判定为空时，抛出错误。
+            error = "数据源模板:工作表 {} 中的单元格 E{} 即[interfaceName]字段的值【{}】不能为空，请填写后重试！".format(self.sheet, self.row + 1,
+                                                                                         self.line[4])
+            log.error(error)
+            raise ValueError(error)
 
-    if row != 0 and line[1].upper() not in ("YES","NO"):
-        error = "数据源模板:工作表 {} 中的单元格 B{} 即[ifCheckSpecial]字段的值【{}】系统未定义或未填写，请检查后重试！".format(sheet, row+1, line[1])
-        log.error(error)
-        raise ValueError(error)
+        if self.row != 0 and not self.line[5]:  # url字段为必填项，当判定为空时，抛出错误。
+            error = "数据源模板:工作表 {} 中的单元格 F{} 即[url]字段的值【{}】不能为空，请填写后重试！".format(self.sheet, self.row + 1, self.line[5])
+            log.error(error)
+            raise ValueError(error)
 
-    if row != 0 and line[2].upper() not in ("YES","NO"):
-        error = "数据源模板:工作表 {} 中的单元格 C{} 即[ifCheckBoundary]字段的值【{}】系统未定义或未填写，请检查后重试！".format(sheet, row+1, line[2])
-        log.error(error)
-        raise ValueError(error)
+        if self.row != 0 and self.line[6].upper() not in ("POST", "GET"):  # method字段为必填项，当判定为空时，抛出错误。
+            error = "数据源模板:工作表 {} 中的单元格 G{} 即[method]字段的值【{}】系统未定义或未填写，请填写后重试！".format(self.sheet, self.row + 1,
+                                                                                       self.line[6])
+            log.error(error)
+            raise ValueError(error)
 
-    if row != 0 and not line[3]: # moduleName字段为必填项，当判定为空时，抛出错误。
-        error = "数据源模板:工作表 {} 中的单元格 D{} 即[moduleName]字段的值【{}】不能为空，请填写后重试！".format(sheet, row+1, line[3])
-        log.error(error)
-        raise ValueError(error)
+        ##############################根据模板中填写的信息进行合成测试用例####################################################
 
-    if row != 0 and not line[4]: # interfaceName字段为必填项，当判定为空时，抛出错误。
-        error = "数据源模板:工作表 {} 中的单元格 E{} 即[interfaceName]字段的值【{}】不能为空，请填写后重试！".format(sheet, row+1, line[4])
-        log.error(error)
-        raise ValueError(error)
+        if self.row == 0:  # 当读到每个self.sheet页的行数为0时，执行写标题操作逻辑
+            self.write_excel_title()
+            return
 
-    if row != 0 and not line[5]: # url字段为必填项，当判定为空时，抛出错误。
-        error = "数据源模板:工作表 {} 中的单元格 F{} 即[url]字段的值【{}】不能为空，请填写后重试！".format(sheet, row+1, line[5])
-        log.error(error)
-        raise ValueError(error)
+        if self.row != 0 and self.line[2].upper() == "YES" and self.line[8] and self.line[10] and (
+                self.line[10] in self.line[8]) and self.line[11] and (
+                self.line[13] or self.line[14]):
+            """
+                * 执行边界值测试时需要满足的条件：
+                * ifCheckSpecial字段填写YES, params、checkParam、fieldType都不可为空，min字段和max字段的值至少有一个不为空，
+                * 且在params字段中的值要包含在checkParam字段的值当中。
+            """
+            log.info("###########################开始执行边界值测试用例书写###############################")
 
-    if row != 0 and line[6].upper() not in ("POST","GET"): # method字段为必填项，当判定为空时，抛出错误。
-        error = "数据源模板:工作表 {} 中的单元格 G{} 即[method]字段的值【{}】系统未定义或未填写，请填写后重试！".format(sheet, row+1, line[6])
-        log.error(error)
-        raise ValueError(error)
+            if self.line[11] == "字符串":
+                log.info("*******************开始执行检查字段是“字符串”类型的边界值测试*************************")
 
-    ##############################根据模板中填写的信息进行合成测试用例####################################################
+            if self.line[11] == "数字":
+                log.info("*******************开始执行检查字段是“数  字”类型的边界值测试*************************")
 
-    if row == 0: # 当读到每个sheet页的行数为0时，执行写标题操作逻辑
-        write_cell_logic.write_excel_title()
-        return
+            if self.line[11] == "手机号":
+                log.info("*******************开始执行检查字段是“手机号”类型的边界值测试*************************")
 
+        if self.row != 0 and self.line[1].upper() == "YES" and self.line[8] and self.line[10] and (
+                self.line[10] in self.line[8]) and self.line[11]:
+            """
+                * 执行特殊字符测试时需要满足的条件：
+                * ifCheckBoundary字段填写 YES ,params、checkParam、fieldType都不可以为空。
+                * 且在params字段中的值要包含在checkParam字段的值当中。
+            """
+            log.info("###########################开始执行特殊字符测试用例书写###############################")
 
-
-    # if row != 0 and line[2].upper() == "YES" and line[8] and line[10] and (line[10] in line[8]) and line[11] and (line[13] or line[14]):
-    #     """
-    #         * 执行边界值测试时需要满足的条件：
-    #         * ifCheckSpecial字段填写YES, params、checkParam、fieldType都不可为空，min字段和max字段的值至少有一个不为空，
-    #         * 且在params字段中的值要包含在checkParam字段的值当中。
-    #     """
-    #     log.info("###########################开始执行边界值测试用例书写###############################")
-    #
-    #     if line[11] == "字符串":
-    #
-    #         log.info("*******************开始执行检查字段是“字符串”类型的边界值测试*************************")
-    #
-    #     if line[11] == "数字":
-    #
-    #         log.info("*******************开始执行检查字段是“数  字”类型的边界值测试*************************")
-    #
-    #     if line[11] == "手机号":
-    #
-    #         log.info("*******************开始执行检查字段是“手机号”类型的边界值测试*************************")
-    #
-    #
-    # if  row != 0 and line[1].upper() == "YES" and line[8] and line[10] and (line[10] in line[8]) and line[11]:
-    #     """
-    #         * 执行特殊字符测试时需要满足的条件：
-    #         * ifCheckBoundary字段填写 YES ,params、checkParam、fieldType都不可以为空。
-    #         * 且在params字段中的值要包含在checkParam字段的值当中。
-    #     """
-    #     log.info("###########################开始执行特殊字符测试用例书写###############################")
-    #
-    #     if line[16]:
-    #         log.info("***************需要测试的特殊字符，有值则允许输入该特殊字符***************")
-    #     else:
-    #         log.info("**************需要测试的特殊字符，无值则允许输入所有特殊字符**************")
-
-
-
-
-
-
-
-
-
-
-# if row == 0:   # 首行
-#     # 读取当前sheet页的总列数
-#     cols = len(line)
-#     log.info("工作表【{}】的首行共有{}列数据".format(sheet,cols))
-#
-#     # 将读取到的第一列列表数据写入到excel文件的第0行。
-#     for index,value in enumerate(line):
-#         log.info("开始向工作表【{0}】中写入表头【列数:{1}--->写入值:{2} 】".format(sheet,index,value))
-#         # writer.write_data(row,index,value)
-#         # 此时根据列数的索引做判断
-#
-# else:
-#     # 功能描述：读指定单元格中的数据进行逻辑处理后并写入到其他行指定的单元格中
-#
-#     for index,value in enumerate(line):
-#         log.info("开始向工作表【{0}】中写入测试用例数据【列数:{1}--->写入值:{2} 】".format(sheet,index,value))
-#         writer.write_data(row,index,value)
+            if self.line[16]:
+                log.info("***************需要测试的特殊字符，有值则允许输入该特殊字符***************")
+            else:
+                log.info("**************需要测试的特殊字符，无值则允许输入所有特殊字符**************")
 
 def auto_generation_case(src_path,save_path):
     # 实例化一个Reader对象
@@ -204,11 +182,11 @@ def auto_generation_case(src_path,save_path):
         reader.set_sheet(sheet)  # 通过sheet页面名称，读取所有的sheet页
         writer.new_sheet(sheet)  # 通过sheet页面名称，新建工作表
 
-        # 在当前工作表页面实例化一个写入逻辑
-        write_cell_logic = WriteCellLogic(writer)
-
         rows = reader.rows  # 读取当前sheet页的总行数
         log.info("工作表【{}】中共有【{}】行数据".format(sheet, rows))
+
+        # 实例化一个在当前sheet上实例化一个写入单元格对象
+        write_cell_logic = WriteCellLogic(writer,sheet,rows)
 
         for row in range(rows):
             log.info("开始读取工作表【{0}】中的第【{1}】行数据...".format(sheet, row))
@@ -216,7 +194,7 @@ def auto_generation_case(src_path,save_path):
             log.info(line)
 
             # 根据读取到的行数据进行写逻辑
-            run_write_excel_logic(line,row,sheet,write_cell_logic)
+            write_cell_logic.run_write_cell_logic(line,row)
 
     writer.save_workbook(save_path)  # 对写入的文件进行保存
 
--- src/common/functions/test.py
+++ src/common/functions/test.py
@@ -7,9 +7,3 @@
 @Version   : 1.0
 @Description: 
 """
-
-
-a = "page=1&size=12&total=0&active=image"
-b = "total"
-if b in a:
-  print(1)
